519f90fca78186fc46a44523e3ef531affd5fb25
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-12 18:39:03 +0300

    catalog/views.py added

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+from .services import messages_saver
+
+
 # Create your views here.
 
 
-
 def index(request):
-    return render(request, "catalog/index.html")
+    """Функция отображения главной страницы"""
+    return render(request, "catalog/index.html", {'nbar': 'home'})
+
+
+def contacts(request):
+    """Функция отображения страницы контактов"""
+    if request.method == "POST":
+        print(request.POST)
+        messages_saver.save_message(request.POST)
+
+    return render(request, "catalog/contacts.html", {'nbar': 'contacts'})
