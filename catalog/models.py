e1ae1a4ad744ff0a4f2106cd4a015b86451bb5ff
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-16 21:02:40 +0300

    Category model added

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+NULLABLE = {"null": True, "blank": True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE)
+
+    def __str__(self):
+        return f"Category <{self.name}>"
+
+
