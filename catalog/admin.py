6aa5f0eeb95ae9e2829311737f4ba6c2a9b1f149
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-17 12:40:22 +0300

    added ordering in catalog models in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,6 +8,7 @@ from catalog.models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name',)
+    ordering = ('pk', )
 
 
 @admin.register(Product)
@@ -15,3 +16,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description')
+    ordering = ('-pk',)
+
