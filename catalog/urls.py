f5bfad5a507b7b442d4c64a00570f8a75bfdc4b7
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-19 14:05:35 +0300

    product controller and template created

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, contacts
+from .views import index, contacts, product
 
 app_name = 'catalog'
 
 urlpatterns = [
     path("", index, name='index'),
     path("contacts/", contacts, name='contacts'),
+    path("product/<int:pk>", product, name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.utils import timezone
 
 from .services import messages_saver
 from .models import Product, Organization
@@ -32,3 +35,20 @@ def contacts(request):
     }
 
     return render(request, "catalog/contacts.html", context)
+
+
+def product(request, pk: int):
+    """Функция отображения страницы продукта"""
+    # product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+
+    # Info for badge "new"
+    is_new = timezone.now() - product.created_at <= datetime.timedelta(days=7)
+
+    context = {
+        'title': f'{product.name}',
+        'product': product,
+        'is_new': is_new
+    }
+
+    return render(request, "catalog/product.html", context)
