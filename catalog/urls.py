8377a82b1887f0ecd5bcd157acdf89487d9e3d59
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-21 00:49:32 +0300

    remove useless path from urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,7 +7,6 @@ app_name = 'catalog'
 
 urlpatterns = [
     path("", index, name='index'),
-    # path("page=<int:page>", index, name='index'),
     path("contacts/", contacts, name='contacts'),
     path("product/<int:pk>/", product, name='product'),
 ]
