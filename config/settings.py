be81c0bae1cb88e0724824929a59e494ea8aaef1
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-24 11:57:44 +0300

    article form created

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     "catalog",
     "backoffice",
     "blog",
@@ -115,3 +118,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
