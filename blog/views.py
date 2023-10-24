b0ce9587fb98d3bba3eb70a13f0a29a5055c6af4
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-24 14:07:33 +0300

    views counter initialized

--- blog/views.py
+++ blog/views.py
@@ -43,3 +43,9 @@ class ArticleUpdateView(UpdateView):
 
 class ArticleDetailView(DetailView):
     model = Article
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
