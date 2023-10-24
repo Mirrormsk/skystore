0b3439fe16bfe9d9006471c4593ba726001ef3d9
Maxim Donchuk <m.donchuk@gmail.com>
2023-10-24 10:47:29 +0300

    add slug to Article

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from catalog.models import TimeStampedModel
 
 class Article(TimeStampedModel):
     title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.CharField(max_length=255, verbose_name='Slug', null=True, blank=True)
     content = models.TextField(verbose_name='Содержимое')
     preview = models.ImageField(upload_to='blog/', verbose_name='Изображение', null=True, blank=True)
     is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
