02f7a9bdc14680edeebc8ef682065e020f704fdc
Maxim Donchuk <m.donchuk@gmail.com>
2023-11-08 15:37:57 +0300

    создана функция check_for_bad_words в миксине BlackListMixin

--- backoffice/forms/product_form.py
+++ backoffice/forms/product_form.py
@@ -8,7 +8,17 @@ from catalog.models import Product
 
 
 class BackListMixin:
-    words_blacklist = ('казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар')
+    words_blacklist = (
+        "казино",
+        "криптовалюта",
+        "крипта",
+        "биржа",
+        "дешево",
+        "бесплатно",
+        "обман",
+        "полиция",
+        "радар",
+    )
     blacklist_ratio_level = 80
 
     def is_similar_words(self, word_1: str) -> Callable:
@@ -22,7 +32,7 @@ class BackListMixin:
 
     @staticmethod
     def remove_punctuation(text: str) -> str:
-        return text.translate(str.maketrans('', '', string.punctuation))
+        return text.translate(str.maketrans("", "", string.punctuation))
 
     def has_word_in_blacklist(self, text: str) -> bool:
         clean_text = self.remove_punctuation(text)
@@ -30,16 +40,24 @@ class BackListMixin:
         result = any(map(self.is_word_in_blacklist, words))
         return result
 
+    def check_for_bad_words(self, data):
+        if self.has_word_in_blacklist(data):
+            raise forms.ValidationError("Текст содержит запрещенное слово")
+
 
 class ProductForm(BackListMixin, forms.ModelForm):
     """Класс для валидации формы создания и изменения продукта"""
 
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price', 'category', 'preview']
+        fields = ["name", "description", "price", "category", "preview"]
 
     def clean_description(self):
-        cleaned_data = self.cleaned_data.get('description')
-        if self.has_word_in_blacklist(cleaned_data):
-            raise forms.ValidationError('Текст содержит запрещенное слово')
+        cleaned_data = self.cleaned_data.get("description")
+        self.check_for_bad_words(cleaned_data)
+        return cleaned_data
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get("name")
+        self.check_for_bad_words(cleaned_data)
         return cleaned_data
