16dfd8df07cd7d84eef61ba8cbba7c31a74ad0cb
Maxim Donchuk <m.donchuk@gmail.com>
2023-11-15 17:57:16 +0300

    добавлены настройки Redis

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,9 @@ LOGOUT_REDIRECT_URL = '/'
 
 LOGIN_URL = reverse_lazy('users:login')
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
