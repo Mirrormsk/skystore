9c1678a3fa39e33d61498340726b9b3f2b21f36f
Maxim Donchuk <m.donchuk@gmail.com>
2023-11-16 10:13:01 +0300

    настроено кеширование контроллера контактов

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import ProductListView, ProductDetailView, ContactsTemplateView
 from .apps import CatalogConfig
@@ -7,6 +8,6 @@ app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name='index'),
-    path("contacts/", ContactsTemplateView.as_view(), name='contacts'),
+    path("contacts/", cache_page(60)(ContactsTemplateView.as_view()), name='contacts'),
     path("product/<int:pk>/", ProductDetailView.as_view(), name='product'),
 ]
